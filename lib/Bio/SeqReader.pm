f424858903cef4898d27529db348ba6a558a1c57
John A. Crow <jac@cpan.org>
2012-09-25 04:13:30 +0000

    Import of JAC/Bio-SeqReader-0.1.3 from CPAN.
    gitpan-cpan-distribution: Bio-SeqReader
    gitpan-cpan-version:      0.1.3
    gitpan-cpan-path:         JAC/Bio-SeqReader-0.1.3.tar.gz
    gitpan-cpan-author:       JAC
    gitpan-cpan-maturity:     released

--- lib/Bio/SeqReader.pm
+++ lib/Bio/SeqReader.pm
@@ -13,7 +13,7 @@ our @ISA         = qw(Exporter);
 our %EXPORT_TAGS = ( 'all' => [qw( )] );
 our @EXPORT_OK   = ( @{ $EXPORT_TAGS{ 'all' } } );
 our @EXPORT      = qw( );
-our $VERSION     = '0.1.2';
+our $VERSION     = '0.1.3';
 
 # Preloaded methods go here.
 
